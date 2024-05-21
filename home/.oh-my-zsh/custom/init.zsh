8d071fee1292633a3d14391949ce270f5ffd24f5
Toby Scott <hi@tobyscott.dev>
2024-05-21 23:30:00 +1000

    add wayland flatpak launch commands

--- home/.oh-my-zsh/custom/init.zsh
+++ home/.oh-my-zsh/custom/init.zsh
@@ -19,3 +19,7 @@ alias k="kubectl"
 ZSH_THEME="frisk"
 
 export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
+
+alias wfp-chrome="flatpak run --socket=wayland com.google.Chrome --enable-features=UseOzonePlatform --ozone-platform=wayland"
+alias wfp-discord="flatpak run --socket=wayland com.discordapp.Discord --enable-features=UseOzonePlatform --ozone-platform=wayland"
+
